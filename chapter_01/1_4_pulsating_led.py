69d8474ef6fe7810f48ab74208f034ab4525f130
Meseth <n.meseth@hs-osnabrueck.de>
2025-09-28 23:26:17 +0200

    Small things

--- chapter_01/1_4_pulsating_led.py
+++ chapter_01/1_4_pulsating_led.py
@@ -2,12 +2,9 @@ import time
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_rgb_led_v2 import BrickletRGBLEDV2
 
-from dotenv import dotenv_values
-config = dotenv_values()  
-
 ipcon = IPConnection()
-ipcon.connect('localhost', 4223) 
-led = BrickletRGBLEDV2(config['UID_LED'], ipcon)
+ipcon.connect("localhost", 4223)
+led = BrickletRGBLEDV2("ZEP", ipcon)
 
 # Turn LED off initially
 led.set_rgb_value(0, 0, 0)
